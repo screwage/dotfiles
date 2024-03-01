13eb37f09c669d786461edc6f982680ccdbd2f62
screwage <code@screwage.net>
2024-03-01 11:41:24 -0300

    cleanup vim-test

--- neovim/.config/nvim/after/plugin/vim-test.lua
+++ neovim/.config/nvim/after/plugin/vim-test.lua
@@ -1,9 +1,3 @@
--- nmap <silent> <leader>t :TestNearest<CR>
--- nmap <silent> <leader>T :TestFile<CR>
--- nmap <silent> <leader>a :TestSuite<CR>
--- nmap <silent> <leader>l :TestLast<CR>
--- nmap <silent> <leader>g :TestVisit<CR>
-
 vim.keymap.set('n', '<leader>tt', vim.cmd.TestNearest, { silent = true })
 vim.keymap.set('n', '<leader>tT', vim.cmd.TestFile, { silent = true })
 vim.keymap.set('n', '<leader>ta', vim.cmd.TestSuite, { silent = true })
