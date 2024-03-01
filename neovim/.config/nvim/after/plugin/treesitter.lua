a2220fdd4d08b0a620540a7c9e358f20eaad4708
screwage <code@screwage.net>
2024-03-01 11:40:03 -0300

    add sql to lsp ensure installed

--- neovim/.config/nvim/after/plugin/treesitter.lua
+++ neovim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "vimdoc", "javascript", "typescript", "python", "c", "go", "rust", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "vimdoc", "javascript", "typescript", "python", "c", "go", "rust", "lua", "vim", "vimdoc", "query", "sql" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
