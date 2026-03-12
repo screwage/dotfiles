3c11e8cd6a402f9f2af5330d30f22dff5b08545f
screwage <code@screwage.net>
2026-03-12 18:53:58 -0300

    feat: add search quickfix hotkey

--- neovim/.config/nvim/lua/dave/lazy.lua
+++ neovim/.config/nvim/lua/dave/lazy.lua
@@ -300,6 +300,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sq', builtin.quickfix, { desc = '[S]earch [Q]uickfix' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
