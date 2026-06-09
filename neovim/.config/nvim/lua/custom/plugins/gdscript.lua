e3ef9d90a5440fa413185aaffe7c7bb68831d205
screwage <code@screwage.net>
2026-06-09 14:14:35 -0300

    feat(nvim): godot setup

--- neovim/.config/nvim/lua/custom/plugins/gdscript.lua
+++ neovim/.config/nvim/lua/custom/plugins/gdscript.lua
@@ -1,13 +1,21 @@
--- local port = os.getenv 'GDScript_Port' or '6005'
--- local cmd = vim.lsp.rpc.connect('127.0.0.1', port)
--- local pipe = '/tmp/godot.pipe'
+-- Based off:
+-- https://simondalvai.org/blog/godot-neovim/
 --
--- vim.lsp.start {
---   name = 'Godot',
---   cmd = cmd,
---   root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
---   on_attach = function(client, bufnr)
---     vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
---   end,
--- }
+---@brief
+---
+--- https://github.com/godotengine/godot
+---
+--- Language server for GDScript, used by Godot Engine.
+--
+local port = os.getenv 'GDScript_Port' or '6005'
+local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
+
+vim.lsp.config('Godot', {
+  cmd = cmd,
+  filetypes = { 'gdscript' },
+  root_markers = { 'project.godot', '.git' },
+})
+
+vim.lsp.enable 'Godot'
+
 return {}
